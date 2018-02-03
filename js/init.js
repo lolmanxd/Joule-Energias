cf1cc56cec368e6b71be217a5db9fe5447fc7bd5
Fabio Mariano <fabiio.mariiano@gmail.com>
2018-02-03 02:26:44 -0300

    finalizando página nossasRealizações

--- js/init.js
+++ js/init.js
@@ -1,8 +1,8 @@
 (function($){
   $(function(){
 
-    $('.sidenav').sidenav();
     $('.parallax').parallax();
+    $('.sidenav').sidenav();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
